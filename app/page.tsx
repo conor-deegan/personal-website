bf812887436c19a7a9c649ceb8fb2241da8e2476
Conor Deegan <conorjdeegan@gmail.com>
2025-04-19 10:56:39 +0200

    updates bio

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ export default function Page() {
     return (
         <section>
             <p className="mb-4">
-                {`I'm a Software Engineer. Mostly interested in Cryptography,
-                Platform Engineering, and AI. I hold an MSc in Distributed
-                Systems and AI.`}
+        I&apos;m a Software Engineer. Mostly interested in Cryptography &
+        Quantum Cryptanalysis. I&apos;m currently head of engineering at{' '}
+                <a href="https://www.projecteleven.com">Project Eleven</a>.
             </p>
             <div className="my-8">
                 <BlogPosts />
