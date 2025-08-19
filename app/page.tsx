b77bd5477f9b28b426347be427360f18bb89ce11
Conor Deegan <conorjdeegan@gmail.com>
2025-08-19 09:37:58 +0100

    updates re writing

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,23 @@ export default function Page() {
         <section>
             <p className="mb-4">
         I&apos;m a computer scientist and software engineer. Mostly interested
-        in applied cryptography. I&apos;m currently co-founder and VP of engineering at{' '}
+        in applied cryptography. I&apos;m co-founder and VP of engineering at{' '}
                 <a
                     href="https://www.projecteleven.com"
                     className="text-blue-500 hover:underline"
+                    target="_blank"
                 >
           Project Eleven
                 </a>
         .
+            </p>
+            <p>At the moment, I write infinitely more over on <a
+                href='https://blog.projecteleven.com'
+                className="text-blue-500 hover:underline"
+                target="_blank"
+            >
+                the Project Eleven blog
+            </a>.
             </p>
             <div className="my-8">
                 <h3 className="text-xl font-bold mb-4">notes</h3>
