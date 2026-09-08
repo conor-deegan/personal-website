e97d89610c9434f241f6903aad33f73755ebc8b7
Conor Deegan <conorjdeegan@gmail.com>
2026-09-08 15:07:21 +0100

    update 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,16 @@
+import Link from "next/link";
+
 export default function NotFound() {
     return (
         <section>
             <h1 className="mb-8 text-2xl font-semibold tracking-tighter">404 - Page Not Found</h1>
-            <p className="mb-4">The page you are looking for does not exist.</p>
+        <p className="mb-4">Nothing here sadly</p>
+        <Link
+            href="/"
+            className="content-link"
+        >
+            Head home
+        </Link>
         </section>
     );
 }
